5906e3a2fe4e422cdef77a1ebb06a63d85fd0009
Ugonz86 <ugonzalez86@gmail.com>
2019-07-18 21:09:28 -0700

    This will commit all the recent updates to the project

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,10 +1,12 @@
 let Promise = require('es6-promise-polyfill').Promise;
+let api = process.env.apiKey;
 // eslint-disable-next-line
     export function practiceLookup(name){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      console.log(name);
-      let url = 'https://api.betterdoctor.com/2016-03-01/practices?name='+name+'&location=47.624569%2C-122.351059%2C100&skip=0&limit=10&user_key=cb0cb401317acaef5e79e512214daf52';
+      console.log(api);
+      let url = 'https://api.betterdoctor.com/2016-03-01/practices?name='+name+'&location=47.624569%2C-122.351059%2C100&skip=0&limit=10&user_key=' + api;
+
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -21,7 +23,7 @@ let Promise = require('es6-promise-polyfill').Promise;
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     console.log(query);
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query='+query+'&location=47.624569%2C-122.351059%2C100&skip=0&limit=10&user_key=cb0cb401317acaef5e79e512214daf52';
+    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query='+query+'&location=47.624569%2C-122.351059%2C100&skip=0&limit=10&user_key=' + api;
     request.onload = function() {
       if (this.status === 200) {
         resolve(request.response);
